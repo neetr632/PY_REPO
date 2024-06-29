d717ee09b98d3abd0708845920bd11c781e760aa
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-06-29 16:53:28 +0000

    yess

--- PYTHON_INEXTURE_CODE/hackerrank/2.py
+++ PYTHON_INEXTURE_CODE/hackerrank/2.py
@@ -1,12 +1,9 @@
+if __name__ == '__main__':
 
-x = int(input())
-y = int(input())
-z = int(input())
-n = int(input())
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
 triplets = [(i, j, k) for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i + j + k) != n]
 print(triplets)
-
-
-# [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 2], [0, 2, 1], [0, 2, 2], 
-# [1, 0, 0], [1, 0, 2], [1, 1, 1], [1, 1, 2], [1, 2, 0], [1, 2, 1], [1, 2, 2], 
-# [2, 0, 1], [2, 0, 2], [2, 1, 0], [2, 1, 1], [2, 1, 2], [2, 2, 0], [2, 2, 1], [2, 2, 2]]
