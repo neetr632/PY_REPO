6852bd12a4a3f94a1b9b34e4eeb164d14c3631c0
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-06-29 14:02:23 +0000

    yes

--- PYTHON_INEXTURE_CODE/hackerrank/2.py
+++ PYTHON_INEXTURE_CODE/hackerrank/2.py
@@ -1,7 +1,12 @@
-# list_comprehensions
+
 x = int(input())
 y = int(input())
 z = int(input())
-n = 3
-triplets = [(i, j, k) for i in range(x) for j in range(y) for k in range(z) if (i + j + k) != n]
+n = int(input())
+triplets = [(i, j, k) for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i + j + k) != n]
 print(triplets)
+
+
+# [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 2], [0, 2, 1], [0, 2, 2], 
+# [1, 0, 0], [1, 0, 2], [1, 1, 1], [1, 1, 2], [1, 2, 0], [1, 2, 1], [1, 2, 2], 
+# [2, 0, 1], [2, 0, 2], [2, 1, 0], [2, 1, 1], [2, 1, 2], [2, 2, 0], [2, 2, 1], [2, 2, 2]]
