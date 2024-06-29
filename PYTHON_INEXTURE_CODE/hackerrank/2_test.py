5ad00b875ff3590f5daf54aa3101fb493367da42
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-06-29 15:36:16 +0000

    message

--- PYTHON_INEXTURE_CODE/hackerrank/2_test.py
+++ PYTHON_INEXTURE_CODE/hackerrank/2_test.py
@@ -6,7 +6,7 @@ n = int(input("Enter n: "))
 
 # Calculate the sum of x, y, and z
 prob_sum = x + y + z
-
+print(prob_sum)
 # Generate triplets (i, j, k) where i < x, j < y, k < z and i + j + k != n
 triplets = [(i, j, k) for i in range(x) for j in range(y) for k in range(z) if (i + j + k) != n]
 
