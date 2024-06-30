887023f3469648e8ed8f3328778eb66baa674e1e
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-06-30 14:13:19 +0000

    yes

--- PYTHON_INEXTURE_CODE/hackerrank/3_try.py
+++ PYTHON_INEXTURE_CODE/hackerrank/3_try.py
@@ -10,17 +10,14 @@ for _ in range(int(input())):
 
 sorted_list = sorted(list_1, key=lambda x: x[1])
 
-# Find the second lowest score
 second_lowest_score = sorted_list[0][1]
 for i in range(1, len(sorted_list)):
     if sorted_list[i][1] > second_lowest_score:
         second_lowest_score = sorted_list[i][1]
         break
 
-# Collect names with the second lowest score
 new_list = [student for student in sorted_list if student[1] == second_lowest_score]
 
-# Sort alphabetically by name and print the names
 new_list.sort()
 for student in new_list:
     print(student[0])
