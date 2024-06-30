2bc704df8fe9ea5befc84c809f421b8c2fb7d2f3
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-06-30 08:18:51 +0000

    yes

--- PYTHON_INEXTURE_CODE/hackerrank/3_list_list.py
+++ PYTHON_INEXTURE_CODE/hackerrank/3_list_list.py
@@ -1,7 +1,15 @@
-list_1 = [["A",12],["B",32],["F",7] ,["G",67], ["H",7],["C",43], ["D",23] ,["E",64]]
-sorted_list = sorted(list_1, key=lambda x:x[1]) 
+list_1 = [
+    ["A", 12],
+    ["B", 32],
+    ["F", 7],
+    ["G", 67],
+    ["H", 7],
+    ["C", 43],
+    ["D", 23],
+    ["E", 64],
+]
+sorted_list = sorted(list_1, key=lambda x: x[1])
 print(sorted_list)
 print(sorted_list[1])
 for x in sorted_list:
-    print(x[1]) 
-    
+    print(x[1])
