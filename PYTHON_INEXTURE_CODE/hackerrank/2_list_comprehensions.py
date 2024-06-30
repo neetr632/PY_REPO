51c69001c058b84c5cf197c162baf87ed4e37c07
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-06-30 07:46:27 +0000

    oka

--- PYTHON_INEXTURE_CODE/hackerrank/2_list_comprehensions.py
+++ PYTHON_INEXTURE_CODE/hackerrank/2_list_comprehensions.py
@@ -7,3 +7,6 @@ if __name__ == '__main__':
     
 triplets = [(i, j, k) for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i + j + k) != n]
 print(triplets)
+
+triplets = [(i, j, k) for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i + j + k) != n]
+print(triplets)
