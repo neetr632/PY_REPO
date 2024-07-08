1f60f32d40fb7587fb3aaa5a13661dcd266f0a12
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-08 12:38:24 +0000

    few

--- PYTHON_INEXTURE_CODE/hackerrank/9_print_fullname.py
+++ PYTHON_INEXTURE_CODE/hackerrank/9_print_fullname.py
@@ -1,7 +1,7 @@
 def print_full_name(first, last):
-    print(f"Hello {first}{last}! You just delved into python.")
+    print(f"Hello {first} {last}! You just delved into python.")
 
 if __name__ == '__main__':
-    first_name = input()
-    last_name = input()
+    first_name = input().strip()
+    last_name = input().strip()
     print_full_name(first_name, last_name)
