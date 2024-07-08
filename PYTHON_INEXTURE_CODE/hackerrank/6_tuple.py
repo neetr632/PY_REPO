545c5ccfb3b9e1f72a46249b81b4f0deaa03ec03
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-08 09:18:02 +0000

    code added

---
+++ PYTHON_INEXTURE_CODE/hackerrank/6_tuple.py
@@ -0,0 +1,9 @@
+tuple = ()
+x = int(input())
+line_input = input().split()
+for i in line_input:
+    tuple = tuple + (int(i),)
+print(tuple)
+    # tuple = tuple + (int(input()),)
+# HasHed_value = hash(tuple)
+# print(HasHed_value)
