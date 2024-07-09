27ca41679cc262101a89f2fa2b4db9789c76ba93
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-09 13:23:46 +0000

    code added

---
+++ PYTHON_INEXTURE_CODE/hackerrank/15_string_mat.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+c = ".|."
+x, y = map(int, input().split())
+
+for i in range(x // 2):
+    line = "-" * (y // 2 - 3 * i) + c * (2 * i + 1) + "-" * (y // 2 - 3 * i)
+    print(line.center(y, "-"))
+print("WELCOME".center(y, "-"))
+for i in range(x // 2 - 1, -1, -1):
+    line = "-" * (y // 2 - 3 * i) + c * (2 * i + 1) + "-" * (y // 2 - 3 * i)
+    print(line.center(y, "-"))
