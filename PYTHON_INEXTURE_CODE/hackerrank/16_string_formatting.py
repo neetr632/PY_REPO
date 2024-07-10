169339893b9cedeb705d8e3d3807d9b120e6bd36
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-10 09:07:14 +0000

    xfv

--- PYTHON_INEXTURE_CODE/hackerrank/16_string_formatting.py
+++ PYTHON_INEXTURE_CODE/hackerrank/16_string_formatting.py
@@ -1,6 +1,8 @@
 def print_formatted(number):
-    for i in range(1, number+1): #range(start, stop, step)
-        print(f"{str(i).ljust(2)} {(oct(i)[2:]).ljust(2)} {((hex(i)[2:]).upper()).ljust(2)} {(bin(i)[2:]).ljust(2)}")  
+    for i in range(1, number+1):
+        binary = bin(number)[2:]
+        length = len(binary)
+        print(f"{str(i).rjust(length)} {(oct(i)[2:]).rjust(length)} {((hex(i)[2:]).upper()).rjust(length)} {(bin(i)[2:]).rjust(length)}")
 
 # n = int(input())
-print_formatted(17)
+print_formatted(33)
