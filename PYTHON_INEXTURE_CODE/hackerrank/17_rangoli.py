86b0a1197a5f173179abb7c8b7bf9554fdfb3f4a
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-10 10:40:17 +0000

    yrs

--- PYTHON_INEXTURE_CODE/hackerrank/17_rangoli.py
+++ PYTHON_INEXTURE_CODE/hackerrank/17_rangoli.py
@@ -1,9 +1,17 @@
 import string
 def print_rangoli(size):
     alphabets = string.ascii_lowercase
-    list_of_alphabets = ",".join(alphabets).split(",")
-    print(list_of_alphabets)
+    list_of_alphabets = list(alphabets)[:size]
+    
     for i in range(size):
-        print("-".join(list_of_alphabets[size-1-i:size]).center(4*size-3, "-"))
-# n = int(input())
-print_rangoli(5)
+        left_part = "-".join(list_of_alphabets[size-i-1:size][::-1] + list_of_alphabets[size-i:size])
+        line = left_part.center(4*size-3, "-")
+        print(line)
+    for i in range(size-1, 0, -1):
+        left_part = "-".join(list_of_alphabets[size-i:size][::-1] + list_of_alphabets[size-i+1:size])
+        line = left_part.center(4*size-3, "-")
+        print(line)
+    
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
