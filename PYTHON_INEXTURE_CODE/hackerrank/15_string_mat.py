c91ff9dea5763c196841b0572708e06d9045c273
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-10 06:00:52 +0000

    yes

--- PYTHON_INEXTURE_CODE/hackerrank/15_string_mat.py
+++ PYTHON_INEXTURE_CODE/hackerrank/15_string_mat.py
@@ -1,5 +1,5 @@
 c = ".|."
-x, y = map(int, input().split())
+x, y = map(int, input().split()) # Since the output is in odd numbers the middle line will be used to print the Welcome Message
 
 for i in range(x // 2):
     line = "-" * (y // 2 - 3 * i) + c * (2 * i + 1) + "-" * (y // 2 - 3 * i)
