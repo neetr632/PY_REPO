13de1bc909d11ee2a21b4e8f58ea8e9416abd144
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-10 06:01:27 +0000

    yrs

--- PYTHON_INEXTURE_CODE/hackerrank/15_string_mat.py
+++ PYTHON_INEXTURE_CODE/hackerrank/15_string_mat.py
@@ -4,7 +4,7 @@ x, y = map(int, input().split()) # Since the output is in odd numbers the middle
 for i in range(x // 2):
     line = "-" * (y // 2 - 3 * i) + c * (2 * i + 1) + "-" * (y // 2 - 3 * i)
     print(line.center(y, "-"))
-    
+
 print("WELCOME".center(y, "-"))
 
 for i in range(x // 2 - 1, -1, -1):
