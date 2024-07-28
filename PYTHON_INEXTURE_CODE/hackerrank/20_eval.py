297a6ea3bf5652b5b8b82b6ae4aed0baa7270921
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-28 13:18:46 +0000

    yes

--- PYTHON_INEXTURE_CODE/hackerrank/20_eval.py
+++ PYTHON_INEXTURE_CODE/hackerrank/20_eval.py
@@ -1,29 +1,21 @@
-import numpy as np
-def create_matrix():
-    dimensions = input().split()
-    rows, cols = int(dimensions[0]), int(dimensions[1])
-    matrix_values = []
-    for _ in range(rows):
-        row_values = list(map(int, input().split()))
-        if len(row_values) != cols:
-            print(f"Warning: Expected {cols} values, but received {len(row_values)}. Please try again.")
-            return None, None  
-        matrix_values.append(row_values)
-    k = int(input())
-    matrix = np.array(matrix_values)
-    return matrix, k  
+rows = 3
+cols = 4
+k = 1
+arr = [
+    [1, 12, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+temp=[]
+for i in arr:
+    temp.append(i[k])
+temp.sort()
 
-def sort_matrix_by_column(matrix, k):
-    sort_indices = np.argsort(matrix[:, k])
-    sorted_matrix = matrix[sort_indices]
-    return sorted_matrix
-
-def print_matrix(matrix):
-    for row in matrix:
-        print(' '.join(map(str, row)))
-
-matrix, k = create_matrix()
-
-if matrix is not None:
-    sorted_matrix = sort_matrix_by_column(matrix, k)
-    print_matrix(sorted_matrix)
+for i in range(len(arr)):
+    arr[i][k] = temp[i]
+    
+print(arr)
+        
+    
+    
+    
