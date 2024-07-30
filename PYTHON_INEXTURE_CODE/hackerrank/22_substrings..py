1c0fef5387f73b2c9aa76a488d87740e66144de6
neetr632 <83157709+neetr632@users.noreply.github.com>
2024-07-30 09:02:42 +0000

    efsef

--- PYTHON_INEXTURE_CODE/hackerrank/22_substrings..py
+++ PYTHON_INEXTURE_CODE/hackerrank/22_substrings..py
@@ -2,12 +2,13 @@ from collections import OrderedDict
 
 
 def merge_the_tools(string, k):
-    od = OrderedDict()
+    substring = []
     for i in range(0, len(string), k):
         substring = string[i : i + k]
+        od = OrderedDict()
         for j in substring:
-            od[j] = j
-        print(od)
+            od[j] = None
+        print(''.join(od.keys()))
 
 
 # if __name__ == '__main__':
